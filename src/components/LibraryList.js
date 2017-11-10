e2601b90d285acb26aacb42fb1110119daeefb02
Andrei Cociug <andrei.ntm@gmail.com>
2017-11-10 17:57:43 +0200

    learn redus

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
+import { View, ListView } from 'react-native';
 
 class LibraryList extends Component {
+    componentWillMount() {
+      const ds = new ListView.DataSource({
+          rowHasChanged: (r1, r2) => r1 !== r2
+      });
+
+      this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow() {
+
+    }
+
     render() {
-        console.log(this.props);
         return (
-            <View></View>
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            ></ListView>
         );
     }
 }
