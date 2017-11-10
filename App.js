98630ee072f8322706fc8e4d09ad278780bc65fb
Andrei Cociug <andrei.ntm@gmail.com>
2017-11-10 17:20:39 +0200

    learn redux

--- App.js
+++ App.js
@@ -3,58 +3,79 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+//
+// import React, {Component} from 'react';
+// import {Text, View} from 'react-native';
+// import firebase from 'firebase';
+// import {Header, Button, Spinner} from './src/components/index';
+// import LoginForm from './src/components/LoginForm';
+//
+// export default class App extends Component {
+//     state = {
+//         loggedIn: null
+//     };
+//
+//     componentWillMount() {
+//         firebase.initializeApp({
+//             apiKey: "AIzaSyD_tLd-2Wg_ggFBCMylTejfc34p0tTPAVM",
+//             authDomain: "react-f6cfd.firebaseapp.com",
+//             databaseURL: "https://react-f6cfd.firebaseio.com",
+//             projectId: "react-f6cfd",
+//             storageBucket: "react-f6cfd.appspot.com",
+//             messagingSenderId: "640136303440"
+//         });
+//
+//         firebase.auth().onAuthStateChanged((user) => {
+//             if(user) {
+//                 this.setState({ loggedIn: true });
+//             } else {
+//                 this.setState({ leggedIn: false });
+//             }
+//         });
+//     }
+//
+//     renderContent() {
+//         switch (this.state.loggedIn) {
+//             case true:
+//                 return (
+//                     <Button onPress={() => firebase.auth().signOut()}>
+//                         Log out
+//                     </Button>
+//                 );
+//             case false:
+//                 return <LoginForm />;
+//             default:
+//                 return <Spinner size="large"/>;
+//         }
+//     }
+//
+//     render() {
+//         return (
+//             <View>
+//                 <Header headerText="Authentication"></Header>
+//                 { this.renderContent() }
+//             </View>
+//         );
+//     }
+// }
 
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import firebase from 'firebase';
-import {Header, Button, Spinner} from './src/components/index';
-import LoginForm from './src/components/LoginForm';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from "./src/components";
+import LibraryList from "./src/components/LibraryList";
 
-export default class App extends Component {
-    state = {
-        loggedIn: null
-    };
-
-    componentWillMount() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyD_tLd-2Wg_ggFBCMylTejfc34p0tTPAVM",
-            authDomain: "react-f6cfd.firebaseapp.com",
-            databaseURL: "https://react-f6cfd.firebaseio.com",
-            projectId: "react-f6cfd",
-            storageBucket: "react-f6cfd.appspot.com",
-            messagingSenderId: "640136303440"
-        });
-
-        firebase.auth().onAuthStateChanged((user) => {
-            if(user) {
-                this.setState({ loggedIn: true });
-            } else {
-                this.setState({ leggedIn: false });
-            }
-        });
-    }
-
-    renderContent() {
-        switch (this.state.loggedIn) {
-            case true:
-                return (
-                    <Button onPress={() => firebase.auth().signOut()}>
-                        Log out
-                    </Button>
-                );
-            case false:
-                return <LoginForm />;
-            default:
-                return <Spinner size="large"/>;
-        }
-    }
-
-    render() {
-        return (
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
             <View>
-                <Header headerText="Authentication"></Header>
-                { this.renderContent() }
+                <Header headerText='Tech Stack'></Header>
+                <LibraryList></LibraryList>
             </View>
-        );
-    }
-}
+        </Provider>
+    );
+};
+
+export default App;
